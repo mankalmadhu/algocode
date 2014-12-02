fe97b861976dd38be91b8fef44ffba26c54deaa7
mahobalan <madhu.ahobalan@amadeus.com>
2014-12-02 19:40:21 +0530

    BST

--- src/main/java/demo/datastructures/bst/BinarySearchTree.java
+++ src/main/java/demo/datastructures/bst/BinarySearchTree.java
@@ -1,28 +1,34 @@
 package demo.datastructures.bst;
 
-import demo.algos.sorting.SortUtil;
-
 public class BinarySearchTree<T extends Comparable> {
 
 	Node root;
 
 	public void put(T elem) {
-		put(elem, root);
+		root = put(elem, root);
 	}
 
-	private void put(T elem, Node parent) {
+	private Node put(T elem, Node x) {
 
-		if (parent == null) {
-			parent = new Node();
-			parent.entity = elem;
+		if (x == null) {
+			x = new Node();
+			x.entity = elem;
+			x.count = 1;
+
 		} else {
-			if (SortUtil.less(elem, parent.entity)) {
-				parent = parent.left;
-			} else {
-				parent = parent.right;
+
+			int compareResult = elem.compareTo(x.entity);
+
+			if (compareResult < 0) {
+				x.left = put(elem, x.left);
+			} else if (compareResult > 0) {
+				x.right = put(elem, x.right);
 			}
-			put(elem, parent);
+
+			x.count = 1 + size(x.left) + size(x.right);
 		}
+
+		return x;
 
 	}
 
@@ -30,21 +36,72 @@ public class BinarySearchTree<T extends Comparable> {
 
 		T retVal = null;
 
-		Node parent = root;
+		Node x = root;
 
-		if (parent != null) {
-			while (parent != null) {
-				if (elem.compareTo(parent) < 0) {
-					parent = parent.left;
-				} else if (elem.compareTo(parent) > 0) {
-					parent = parent.right;
-				} else {
-					retVal = parent.entity;
-				}
+		while (x != null) {
+
+			int compareResult = elem.compareTo(x.entity);
+
+			if (compareResult < 0) {
+				x = x.left;
+			} else if (compareResult > 0) {
+				x = x.right;
+			} else {
+				retVal = x.entity;
+				break;
 			}
 		}
 
 		return retVal;
+
+	}
+
+	public void printTree() {
+
+		if (root != null) {
+			System.out.println("Root Is :" + root.entity);
+		}
+		printTree(root);
+	}
+
+	private void printTree(Node x) {
+
+		if (x == null) {
+			return;
+		}
+
+		printTree(x.left);
+		System.out.println(x);
+		printTree(x.right);
+
+	}
+
+	public int size() {
+
+		return size(root);
+	}
+
+	private int size(Node x) {
+		int size = 0;
+		if (x != null) {
+			size = x.count;
+		}
+
+		return size;
+
+	}
+
+	public int height() {
+		return height(root);
+	}
+
+	private int height(Node x) {
+
+		if (x == null) {
+			return -1;
+		}
+
+		return 1+ Math.max(height(x.left), height(x.right));
 
 	}
 
@@ -57,6 +114,26 @@ public class BinarySearchTree<T extends Comparable> {
 		Node right;
 
 		int count;
+
+		@Override
+		public String toString() {
+
+			return this.entity + ":" + count;
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		int[] input = new int[] { 4, 6, 2, 5, 1, 3 };
+
+		BinarySearchTree<Integer> bt = new BinarySearchTree<Integer>();
+
+		for (int i : input) {
+			bt.put(i);
+		}
+
+		System.out.println(bt.height());
 
 	}
 }
