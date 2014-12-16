783356065c84b0d6522098dee642307ab8e48061
mankalmadhu <mankalmadhu@gmail.com>
2014-12-16 23:11:20 +0530

    C

--- src/main/java/demo/datastructures/bst/BinarySearchTree.java
+++ src/main/java/demo/datastructures/bst/BinarySearchTree.java
@@ -1,5 +1,10 @@
 package demo.datastructures.bst;
 
+/**
+ * @author mankalmadhu
+ *
+ * @param <T>
+ */
 public class BinarySearchTree<T extends Comparable<T>> {
 
 	Node root;
@@ -116,6 +121,20 @@ public class BinarySearchTree<T extends Comparable<T>> {
 
 	}
 
+	/**
+	 * 
+	 * 1) If compareResult is '0' then 'x' is floor.
+	 * 
+	 * 2) If compareResult is <0 then search in left subtree.
+	 * 
+	 * 3) If compareResult is >0. Store current node in temp. See if right
+	 * subtree has a minimum recursively. Rescursion will take care to go left
+	 * or right accordingly.
+	 * 
+	 * @param elem
+	 * @param x
+	 * @return
+	 */
 	public Node floor(T elem, Node x) {
 		Node retVal = null;
 
@@ -152,12 +171,15 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		}
 	}
 
-	public T min() {
+	public Node min(Node x) {
 
-		Node x = root;
+		if (x == null) {
+			x = root;
+		}
+
 		while (true) {
 			if (x.left == null) {
-				return x.entity;
+				return x;
 			}
 			x = x.left;
 		}
@@ -183,6 +205,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 
 	/**
 	 * Kth minimum element
+	 * 
 	 * @param k
 	 * @return
 	 */
@@ -200,6 +223,96 @@ public class BinarySearchTree<T extends Comparable<T>> {
 			return select(k - leftSubTreeNodes - 1, x.right);
 		} else {
 			return x.entity;
+		}
+
+	}
+
+	public T deleteMin() {
+		return deleteMin(root).entity;
+	}
+
+	/**
+	 * 1) Find element with no left child.
+	 * 
+	 * 2) Return the right element of the node (which would become left node of
+	 * the parent due to recursive call:x.left = deleteMin(x.left);
+	 * 
+	 * 3) After final recursion the element itself has no reference in tree.
+	 * 
+	 * 4) If x.right is null then x.left in higher recursive call becomes null,
+	 * effectively deleting the element.
+	 * 
+	 * @param x
+	 * @return
+	 */
+	private Node deleteMin(Node x) {
+		if (x.left == null) {
+			return x.right;
+		}
+
+		x.left = deleteMin(x.left);
+		x.count = 1 + size(x.left) + size(x.right);
+
+		return x;
+	}
+
+	public void delete(T entity) {
+		delete(root, entity);
+	}
+
+	/**
+	 * Covers 3 cases :
+	 * 
+	 * 1) Node has no children : final recursion returns null so in higher
+	 * recursion assignment x.left = delete(x.left, entity) (or x.right) would
+	 * be set as 'null'. So effectively node is deleted.
+	 * 
+	 * 2) Node has one children. Similar to above. In this case the non-null
+	 * child would be assigned to 'x.left' or 'x.right' in final recursion and
+	 * 'x' is effectively deleted.
+	 * 
+	 * 3) Node has two children :
+	 * 
+	 * a) Store the node in temp 't'.
+	 * 
+	 * b) Find the successor of 't' (i.e. a min call to t.right subtree).Point
+	 * 'x' to successor.
+	 * 
+	 * c) Call deleteMin operation on t.right subtree and the returned node
+	 * would be x.right.Whatever on right before delete for successor would be back on right again.
+	 * 
+	 * d) x.left would be t.left(No changes in left subtree).
+	 * 
+	 * 
+	 * @param x
+	 * @param entity
+	 * @return
+	 */
+	public Node delete(Node x, T entity) {
+
+		int compareResult = entity.compareTo(x.entity);
+
+		if (compareResult < 0) {
+			return x.left = delete(x.left, entity);
+		} else if (compareResult > 0) {
+			return x.right = delete(x.right, entity);
+		} else {
+
+			if (x.left == null) {
+				return x.right;
+			} else if (x.right == null) {
+				return x.left;
+			}
+
+			Node t = x;
+
+			x = min(t.right);
+			x.right = deleteMin(t.right);
+			x.left = t.left;
+
+			x.count = 1 + size(x.left) + size(x.right);
+
+			return x;
 		}
 
 	}
@@ -234,7 +347,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 
 		System.out.println(bt.height());
 		System.out.println(bt.max());
-		System.out.println(bt.min());
+		System.out.println(bt.min(null));
 		System.out.println(bt.select(5));
 
 	}
